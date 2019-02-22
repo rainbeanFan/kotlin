2d1fd7537f8c7051d220d81c8c75d97ad3f4178d
Ilya Kirillov <ilya.kirillov@jetbrains.com>
2019-02-22 15:36:19 +0300

    Show warnings for Java methods with external annotations when they called from Kotlin code

---
+++ idea/testData/externalAnnotations/ClassWithExternalAnnotatedMembers.java
@@ -0,0 +1,11 @@
+public class ClassWithExternalAnnotatedMembers {
+    String nullableMethod() {
+        return null;
+    }
+
+    String notNullMethod() {
+        return "nya";
+    }
+
+    String nullableField = "nya";
+}
