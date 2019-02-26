f8783bf830ffcdcc5d5891951134260a1982bd2a
Ilya Chernikov <ilya.chernikov@jetbrains.com>
2019-02-26 11:21:00 +0100

    Add a new module for scripting infrastructure fir compiler...
    add embeddable variant as well

--- plugins/scripting/scripting-compiler-embeddable/build.gradle.kts
+++ plugins/scripting/scripting-compiler-embeddable/build.gradle.kts
@@ -1,15 +1,12 @@
 import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
-import org.jetbrains.kotlin.pill.PillExtension.Variant
 
 plugins { java }
 
-description = "Kotlin Scripting Compiler Plugin for embeddable compiler"
+description = "Kotlin Compiler Infrastructure for Scripting for embeddable compiler"
 
 val packedJars by configurations.creating
 dependencies {
-    packedJars(project(":kotlin-scripting-compiler")) { isTransitive = false }
-    packedJars(project(":kotlin-scripting-common")) { isTransitive = false }
-    packedJars(project(":kotlin-scripting-jvm")) { isTransitive = false }
+    packedJars(project(":kotlin-scripting-compiler-impl")) { isTransitive = false }
 }
 
 publish()
---
+++ plugins/scripting/scripting-compiler-impl-embeddable/build.gradle.kts
@@ -0,0 +1,22 @@
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
+
+plugins { java }
+
+description = "Kotlin Scripting Compiler Plugin for embeddable compiler"
+
+val packedJars by configurations.creating
+dependencies {
+    packedJars(project(":kotlin-scripting-compiler")) { isTransitive = false }
+    packedJars(project(":kotlin-scripting-common")) { isTransitive = false }
+    packedJars(project(":kotlin-scripting-jvm")) { isTransitive = false }
+}
+
+noDefaultJar()
+runtimeJar(rewriteDepsToShadedCompiler(
+    task<ShadowJar>("shadowJar")  {
+        from(packedJars)
+    }
+))
+sourcesJar()
+javadocJar()
+publish()
