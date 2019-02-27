e3c48d7ad828812fc6b6ce7ebad317356be146a5
Kirill Shmakov <kirill.shmakov@jetbrains.com>
2019-02-27 18:30:44 +0300

    Copyrights updated.

--- libraries/tools/kotlin-gradle-plugin-integration-tests/src/test/resources/testProject/new-mpp-native-binaries/kotlin-dsl/exported/src/commonMain/kotlin/exported.kt
+++ libraries/tools/kotlin-gradle-plugin-integration-tests/src/test/resources/testProject/new-mpp-native-binaries/kotlin-dsl/exported/src/commonMain/kotlin/exported.kt
@@ -1,13 +1,3 @@
-/*
- * Copyright 2010-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license
- * that can be found in the license/LICENSE.txt file.
- */
-
 package com.example
 
-import com.example.exported
-
-fun main() {
-    val exp = exported()
-    println("foo.main: exp = $exp")
-}
+fun exported() = 42
